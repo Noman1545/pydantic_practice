5df687e21ee3539d05e4f58c0adff57d0161d0c2
noman asim <asimnoman30@gmail.com>
2026-08-30 03:31:55 +0500

    nested basemodel

---
+++ nested_basemodel.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+class Adress(BaseModel):
+    house:int
+    street:int
+    block:str
+    
+class patient(BaseModel):
+    name:str
+    age:int
+    adress:Adress
+    
+adress_info={'house':389,'street':14,'block':'DHA'}
+adress1=Adress(**adress_info)
+patient_info={ 'name':'noman','age':22,'adress':adress1}
+patient1=patient(**patient_info)
+
+def show(p:patient):
+    print(p.name)
+    print(p.age)
+    print(p.adress)
+    print(p.adress.house)
+    print(p.adress.block)
+show(patient1)
